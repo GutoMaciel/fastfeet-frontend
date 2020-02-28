6b19313fed531d4fde7e99b7b6d4e46b74441129
Gustavo Maciel dos Santos <gustavomaciel@Gustavos-MacBook-Pro.local>
2020-02-28 13:48:20 -0300

    unform and sign in validation

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,20 +1,32 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.png';
 
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Sign with a valid email')
+    .required('Email required'),
+  password: Yup.string().required('Password required'),
+});
+
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
   return (
     <>
       <img src={logo} alt="FastFeet" />
 
-      <form>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <strong>YOUR EMAIL</strong>
-        <input type="email" placeholder="Email" />
+        <Input name="email" type="email" placeholder="Email" />
         <strong>YOUR PASSWORD</strong>
-        <input type="password" placeholder="Password" />
+        <Input name="password" type="password" placeholder="Password" />
 
         <button type="submit">Sign In</button>
-      </form>
+      </Form>
     </>
   );
 }
